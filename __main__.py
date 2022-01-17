2c64159f447a6406c5a8872ae65f215df21c82af
Chris Lynch <chris.lynch@datacom.co.nz>
2022-01-17 15:45:41 +1300

    add bucket website config

--- __main__.py
+++ __main__.py
@@ -1,16 +1,22 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
 from pulumi_aws import s3
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# create resources
+bucket = s3.Bucket(
+  'my-bucket',
+  website=s3.BucketWebsiteArgs(
+    index_document='index.html'
+  )
+)
 
 bucket_object = s3.BucketObject(
   'index.html',
+  acl='public-read',
+  content_type='text/html',
   bucket=bucket.id,
   source=pulumi.FileAsset('index.html')
 )
 
-# Export the name of the bucket
+# exports
 pulumi.export('bucket_name', bucket.id)
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
