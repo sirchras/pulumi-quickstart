1c9e4b5a50d0ce3266cea43e950dac8c73d4eace
Chris Lynch <chris.lynch@datacom.co.nz>
2022-01-19 20:26:40 +1300

    rewrite stack
    replace bucket with webserver

--- __main__.py
+++ __main__.py
@@ -1,22 +1,28 @@
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
 # create resources
-bucket = s3.Bucket(
-  'my-bucket',
-  website=s3.BucketWebsiteArgs(
-    index_document='index.html'
-  )
+ami = aws.ec2.get_ami(
+  most_recent=True,
+  owners=['137112412989'],
+  filters=[{'name': 'name', 'values': ['amzn-ami-hvm-*']}]
 )
 
-bucket_object = s3.BucketObject(
-  'index.html',
-  acl='public-read',
-  content_type='text/html',
-  bucket=bucket.id,
-  source=pulumi.FileAsset('index.html')
+group = aws.ec2.SecurityGroup(
+  'webserver-sg',
+  description='enable ssh access',
+  ingress=[
+    {'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['202.27.76.248/32']}
+  ]
+)
+
+server = aws.ec2.Instance(
+  'webserver',
+  instance_type='t2.micro',
+  vpc_security_group_ids=[group.id],
+  ami=ami.id
 )
 
 # exports
-pulumi.export('bucket_name', bucket.id)
-pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
+pulumi.export('public_ip', server.public_ip)
+pulumi.export('public_hostname', server.public_dns)
