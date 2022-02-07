cd99db546c9b04e4a26717d3d030dbd6a0b86c27
sirchras <cjlynch@hotmail.co.nz>
2022-02-07 14:04:21 +1300

    refactor stack

--- __main__.py
+++ __main__.py
@@ -1,46 +1,10 @@
 import pulumi
-import pulumi_aws as aws
 
-# config
-config = pulumi.Config()
-
-ip_address = config.require_secret('ip_address')
-key_pair = config.require_secret('key_pair')
-
-# create resources
-ami = aws.ec2.get_ami(
-  most_recent=True,
-  owners=['137112412989'],
-  filters=[{'name': 'name', 'values': ['amzn-ami-hvm-*']}]
-)
-
-group = aws.ec2.SecurityGroup(
-  'webserver-sg',
-  description='enable ssh access',
-  ingress=[
-    {'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': [ip_address]},
-    {'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0']}
-  ]
-)
-
-user_data = """
-#!/bin/bash
-echo 'Hello, World!' > index.html
-nohup python -m SimpleHTTPServer 80 &
-"""
-
-server = aws.ec2.Instance(
-  'webserver',
-  instance_type='t2.micro',
-  vpc_security_group_ids=[group.id],
-  user_data=user_data,
-  ami=ami.id,
-  key_name=key_pair,
-  tags={
-    'Name': 'webserver'
-  }
-)
+# create a new stack
+import infra
 
 # exports
-pulumi.export('public_ip', server.public_ip)
-pulumi.export('public_hostname', server.public_dns)
+pulumi.export('group_id', infra.group.id)
+pulumi.export('server_id', infra.server.id)
+pulumi.export('public_ip', infra.server.public_ip)
+pulumi.export('public_hostname', infra.server.public_dns)
---
+++ infra.py
@@ -0,0 +1,42 @@
+import pulumi
+import pulumi_aws as aws
+
+# config
+config = pulumi.Config()
+
+ip_address = config.require_secret('ip_address')
+key_pair = config.require_secret('key_pair')
+
+# create resources
+group = aws.ec2.SecurityGroup(
+  'webserver-sg',
+  description='enable ssh access',
+  ingress=[
+    {'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': [ip_address]},
+    {'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0']}
+  ]
+)
+
+ami = aws.ec2.get_ami(
+  most_recent=True,
+  owners=['137112412989'],
+  filters=[{'name': 'name', 'values': ['amzn-ami-hvm-*']}]
+)
+
+user_data = """
+#!/bin/bash
+echo 'Hello, World!' > index.html
+nohup python -m SimpleHTTPServer 80 &
+"""
+
+server = aws.ec2.Instance(
+  'webserver',
+  instance_type='t2.micro',
+  vpc_security_group_ids=[group.id],
+  user_data=user_data,
+  ami=ami.id,
+  key_name=key_pair,
+  tags={
+    'Name': 'webserver'
+  }
+)
